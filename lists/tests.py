d65a6411481d18eb8c060dd16dce84178bede43c
Pawel Podberezsky <podberezsky.pawel@yandex.by>
2018-10-15 11:28:20 +0300

    Модель для элементов списка и связанная с ней миграция

--- lists/tests.py
+++ lists/tests.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from lists.models import Item
 
 # Create your tests here.
 
@@ -16,3 +17,25 @@ class HomePageTest(TestCase):
         response = self.client.post('/', data={'item_text': 'A new list item'})
         self.assertIn('A new list item', response.content.decode())
         self.assertTemplateUsed(response, 'home.html')
+
+
+class ItemModelTest(TestCase):
+    ''' тест модели элементов списка '''
+
+    def test_saving_and_retrieving_items(self):
+        ''' тест сохранения и получения элементов списка '''
+        first_item = Item()
+        first_item.text = 'The first (ever) list item'
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = 'The second  list item'
+        second_item.save()
+
+        saved_item = Item.objects.all()
+        self.assertEqual(saved_item.count(), 2)
+
+        first_saved_item  =  saved_item[0]
+        second_saved_item  =  saved_item[1]
+        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+        self.assertEqual(second_saved_item.text, 'The second  list item')
