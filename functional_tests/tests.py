2a1ed8f202915bc4c83ab77cb07858583c481cd3
Pawel Podberezsky <podberezsky.pawel@yandex.by>
2018-11-10 12:02:11 +0300

    Первые шаги для макетирования

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -114,4 +114,32 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertIn('Buy milk', page_text)
 
         # Satisfied, they both go back to sleep
+    def test_layout_and_styling(self):
+        ''' тест макета и стилевого оформления '''
+        # Эдит открывает домашнюю страницу
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        #Она замечает, что поле ввода аккуратно центрировано
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width']/2,
+            512,
+            delta=10
+
+        )
+
+        # Она начинает новый список и видит, что поле ввода там тоже
+        # аккуратно центриолвано
+        inputbox.send_keys('testing')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: testing')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width']/2,
+            512,
+            delta=10
+
+        )
+
 
