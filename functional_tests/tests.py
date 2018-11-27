4e9f7bac16d6c86d6f0f43a63068b44b0e23dfe0
Pawel Podberezsky <podberezsky.pawel@yandex.by>
2018-11-27 07:42:27 +0300

    Перемещена база данных sqlite за пределы главного дерева исходного кода

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.common.keys import Keys
 import time
+import os
 
 MAX_WAIT = 10
 
@@ -11,10 +12,12 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
+        staging_server = os.environ.get('STAGING_SERVER')
+        if staging_server:
+            self.live_server_url = 'http://' + staging_server
 
     def tearDown(self):
         self.browser.quit()
-
 
     def wait_for_row_in_list_table(self, row_text):
         start_time = time.time()
@@ -28,7 +31,6 @@ class NewVisitorTest(StaticLiveServerTestCase):
                 if time.time() - start_time > MAX_WAIT:
                     raise e
                 time.sleep(0.5)
-
 
     def test_can_start_a_list_for_one_user(self):
         # Edith has heard about a cool new online to-do app. She goes
@@ -68,7 +70,6 @@ class NewVisitorTest(StaticLiveServerTestCase):
         self.wait_for_row_in_list_table('1: Buy peacock feathers')
 
         # Satisfied, she goes back to sleep
-
 
     def test_multiple_users_can_start_lists_at_different_urls(self):
         # Edith starts a new to-do list
@@ -120,7 +121,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
         self.browser.get(self.live_server_url)
         self.browser.set_window_size(1024, 768)
 
-        #Она замечает, что поле ввода аккуратно центрировано
+        # Она замечает, что поле ввода аккуратно центрировано
         inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertAlmostEqual(
             inputbox.location['x'] + inputbox.size['width']/2,
@@ -141,5 +142,3 @@ class NewVisitorTest(StaticLiveServerTestCase):
             delta=10
 
         )
-
-
