259265fefe6d387285545b6d225729401b6cadc3
pol1969 <podberezsky1969@gmail.com>
2019-01-14 21:31:17 +0300

    Реализована валидация наличия повторяющегося элемента на уровне модели

--- lists/models.py
+++ lists/models.py
@@ -8,11 +8,20 @@ class List(models.Model):
     def get_absolute_url(self):
         ''' получить абсолютный url '''
         return reverse('view_list', args=[self.id])
-
+    
 class Item(models.Model):
     ''' элемент списка '''
-    text = models.TextField(default='')
+    text = models.TextField(default='' )
     list = models.ForeignKey(List, default=None, on_delete=models.CASCADE)
     #on_delete=models.CASCADE) добавлено мною
+
+    def __str__(self):
+        return self.text
+
+
+    class Meta:
+        ordering = ('id',)
+        unique_together = ('list','text')
+
 
 
--- lists/tests/test_models.py
+++ lists/tests/test_models.py
@@ -3,40 +3,8 @@ from lists.models import Item, List
 from django.core.exceptions import ValidationError
 # Create your tests here.
 
-
-
-
-class ListAndItemModelTest(TestCase):
+class ItemModelTest(TestCase):
     ''' тест модели элементов списка '''
-
-    def test_saving_and_retrieving_items(self):
-        ''' тест сохранения и получения элементов списка '''
-
-        list_ = List()
-        list_.save()
-
-        first_item = Item()
-        first_item.text = 'The first (ever) list item'
-        first_item.list = list_
-        first_item.save()
-
-        second_item = Item()
-        second_item.text = 'The second  list item'
-        second_item.list = list_
-        second_item.save()
-
-        saved_list = List.objects.first()
-        self.assertEquals(saved_list, list_)
-
-        saved_item = Item.objects.all()
-        self.assertEqual(saved_item.count(), 2)
-
-        first_saved_item = saved_item[0]
-        second_saved_item = saved_item[1]
-        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
-        self.assertEqual(first_saved_item.list, list_)
-        self.assertEqual(second_saved_item.text, 'The second  list item')
-        self.assertEqual(second_saved_item.list, list_)
 
     def test_cannot_save_empty_items(self):
         ''' тест: нельзя добавлять пустые элементы списка '''
@@ -45,6 +13,56 @@ class ListAndItemModelTest(TestCase):
         with self.assertRaises(ValidationError):
             item.save()
             item.full_clean()
+
+    def test_duplicate_items_are_invalid(self):
+        '''тест: повторы элементов недопустимы '''
+        list_ =  List.objects.create()
+        Item.objects.create(list=list_, text='bla')
+        with self.assertRaises(ValidationError):
+            item = Item(list=list_, text='bla')
+            item.full_clean()
+            #item.save()
+
+    def tetst_CAN_save_same_item_to_different_lists(self):
+        list1 = List.object.create()
+        list2 = List.object.create()
+        Item.objects.create(list=list1, text='bla')
+        item = Item(list=list2, text='bla')
+        item.full_clean() #не должен поднять исключение
+    
+    def test_list_ordering(self):
+        ''' тест упорядочения списка '''
+        list1 = List.objects.create()
+        item1 = Item.objects.create(list=list1, text='i1')
+        item2 = Item.objects.create(list=list1, text='item2')
+        item3 = Item.objects.create(list=list1, text='3')
+        self.assertEqual(
+                list(Item.objects.all()),
+                [item1, item2, item3]
+                )
+            
+    def test_string_representation(self):
+        ''' тест строкового представления '''
+        item = Item(text='some text')
+        self.assertEqual(str(item), 'some text')
+
+    def test_default_text(self):
+        '''тест заданного по умолчанию текста '''
+        item = Item()
+        self.assertEqual(item.text, '')
+
+    def test_item_is_related_to_list(self):
+        '''тест: элемент связан со списком '''
+        list_ = List.objects.create()
+        item = Item()
+        item.list = list_
+        item.save()
+        self.assertIn(item, list_.item_set.all())
+
+
+class ListModelTest(TestCase):
+    ''' тест модели списка '''
+    
     def test_get_absolute_url(self):
         ''' тест: получен абсолютный url '''
         list_ = List.objects.create()
