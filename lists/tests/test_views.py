824853e73b46e61fdb13e4fbe70865f22a3d1b99
pol1969 <podberezsky1969@gmail.com>
2019-01-24 05:22:28 +0300

    Глава 15 страница 310

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 from lists.models import Item, List
 from django.utils.html import escape
 from lists.forms import ItemForm, EMPTY_ITEM_ERROR
+from unittest import skip
 
 # Create your tests here.
 
@@ -135,6 +136,21 @@ class ListViewTest(TestCase):
         response = self.client.get(f'/lists/{list_.id}/')
         self.assertIsInstance(response.context['form'], ItemForm)
         self.assertContains(response, 'name="text"')
+
+    @skip
+    def test_for_invalid_input_error_end_up_on_list_page(self):
+        '''тест: ошибки валидации повторяющегося элемента 
+        оканчиваются на странице списков '''
+        list1 = List.objects.create()
+        item1 = Item.objects.create(list=list1, text='textey')
+        response = self.client.post(
+                f'/lists/{list1.id}/',
+                data={'text':'textey'}
+                )
+        expected_error = escape("You've already got this in your list")
+        self.assertContains(response, espected_error)
+        self.assertTemplateUsed(response, 'list.html')
+        self.assertEqual(Item.objects.all().count(),1)
 
 
 
